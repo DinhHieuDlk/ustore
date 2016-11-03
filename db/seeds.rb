bb99cbb85c713f6878be9f824276bc6b6133dd62
Dinh Hieu <dinhhieu.dlk@gmail.com>
2016-11-03 15:20:57 +0700

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-5.times do 
-	categories =Category.create(name: Faker::Commerce.department, image: Faker::Avatar.image)
-	@category = Category.all
-end
 
-10.times do
-	@category.each do |index|
-	products =Product.create(
-		name: Faker::Commerce.product_name,
-		price: Faker::Commerce.price,
-		description: Faker::Lorem.paragraph,
-		image: Faker::Avatar.image,
-		category_id: index.id)
-end
-end
 User.create!(email: 'dinhhieu.dlk@gmail.com', password: 'dinhhieu', password_confirmation: 'dinhhieu' ,admin: 'true')
